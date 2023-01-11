c1caa349d0f3df2047f22699d46c6d806f1c2311
timst <timstar7777@gmail.com>
2023-01-11 21:21:59 +0200

    add Kiber0401CW

--- src/KiberCW/Training1101.java
+++ src/KiberCW/Training1101.java
@@ -4,23 +4,31 @@ import java.util.Scanner;
 
 public class Training1101 {
     public static void main(String[] args) {
-        System.out.println(converterFromCelciusToFahrenheit(10));
+        System.out.println(compareMagnitudeOfNumbers(5, -8));
 
 
         System.out.println("Введите градусы по цельсию: ");
         Scanner scan = new Scanner(System.in);
-        int number = scan.nextInt();
-        System.out.println(converterFromCelciusToKelvin(number));
-
+        int userTemperatureInCelcius = scan.nextInt();
+        System.out.println(userTemperatureInCelcius + " C = " + converterFromCelciusToFahrenheit(userTemperatureInCelcius) + " F ");
+        System.out.println(userTemperatureInCelcius + " C = " + converterFromCelciusToKelvin(userTemperatureInCelcius) + " K ");
+        // 10 C = 50 F
+        // 10 C = 283 K
         // 10 C = 50 F
         // 10 C = 283 K
     }
-
     public static int converterFromCelciusToFahrenheit(int celcius) {
-        return celcius * 9 / 5 + 32;
+            return celcius * 9 / 5 + 32;
     }
+    public static float converterFromCelciusToKelvin(int celcius) {
+        return celcius + 273;
+    }
+    public static int compareMagnitudeOfNumbers(int firstNumber, int secondNumber) {
+        if (Math.abs(firstNumber) > Math.abs(secondNumber)){
+            return firstNumber;
+        }  else {
+            return secondNumber;
+        }
 
-    public static float converterFromCelciusToKelvin(int number) {
-        return number + 273;
     }
 }
